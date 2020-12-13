f88fefd1368a19346f5d9985ab27e4adba81f798
Subham Gaurav <shubham.gaurav.1994@gmail.com>
2020-12-13 03:15:08 -0500

    pushing the final models to github

---
+++ vae_ffnn.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import norm
+
+from keras.layers import Input, Dense, Lambda
+from keras.models import Model
+from keras import backend as K
+from keras.datasets import mnist
+
+
+batch_size = 100
+original_dim = 784
+latent_dim = 2
+intermediate_dim = 256
+epochs = 50
+
+
+(x_train, y_train_), (x_test, y_test_) = mnist.load_data()
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+
+
+x = Input(shape=(original_dim,))
+h = Dense(intermediate_dim, activation='relu')(x)
+
+z_mean = Dense(latent_dim)(h)
+z_log_var = Dense(latent_dim)(h)
+
+def sampling(args):
+    z_mean, z_log_var = args
+    epsilon = K.random_normal(shape=K.shape(z_mean))
+    return z_mean + K.exp(z_log_var / 2) * epsilon
+
+z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
+
+decoder_h = Dense(intermediate_dim, activation='relu')
+decoder_mean = Dense(original_dim, activation='sigmoid')
+h_decoded = decoder_h(z)
+x_decoded_mean = decoder_mean(h_decoded)
+
+vae = Model(x, x_decoded_mean)
+
+xent_loss = K.sum(K.binary_crossentropy(x, x_decoded_mean), axis=-1)
+kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
+vae_loss = K.mean(xent_loss + kl_loss)
+
+vae.add_loss(vae_loss)
+vae.compile(optimizer='rmsprop')
+vae.summary()
+
+vae.fit(x_train,
+        shuffle=True,
+        epochs=epochs,
+        batch_size=batch_size,
+        validation_data=(x_test, None))
+
+
+encoder = Model(x, z_mean)
+
+x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
+plt.figure(figsize=(6, 6))
+plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test_)
+plt.colorbar()
+plt.show()
+
+decoder_input = Input(shape=(latent_dim,))
+_h_decoded = decoder_h(decoder_input)
+_x_decoded_mean = decoder_mean(_h_decoded)
+generator = Model(decoder_input, _x_decoded_mean)
+
+n = 15
+digit_size = 28
+figure = np.zeros((digit_size * n, digit_size * n))
+
+grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
+grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
+
+for i, yi in enumerate(grid_x):
+    for j, xi in enumerate(grid_y):
+        z_sample = np.array([[xi, yi]])
+        x_decoded = generator.predict(z_sample)
+        digit = x_decoded[0].reshape(digit_size, digit_size)
+        figure[i * digit_size: (i + 1) * digit_size,
+               j * digit_size: (j + 1) * digit_size] = digit
+
+plt.figure(figsize=(10, 10))
+plt.imshow(figure, cmap='Greys_r')
+plt.show()
