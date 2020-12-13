cb1d950c5e4bc0adff84e195182d179cd4308ea6
Subham Gaurav <shubham.gaurav.1994@gmail.com>
2020-12-13 13:50:55 -0500

    remove plots and refactor to methods

--- gan_ffnn.py
+++ gan_ffnn.py
@@ -1,66 +1,68 @@
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.layers import BatchNormalization
 from tensorflow.keras.layers import Input, Dense, Reshape, Flatten
-from tensorflow.keras.layers import BatchNormalization, Activation, ZeroPadding2D
 from tensorflow.keras.layers import LeakyReLU
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.datasets import mnist
-import matplotlib.pyplot as plt
-
-import os
-import numpy as np
 
 
-class GAN():
+class GAN:
     def __init__(self):
-        self.img_rows = 28
-        self.img_cols = 28
-        self.channels = 1
-        self.img_shape = (self.img_rows, self.img_cols, self.channels)
-        self.latent_dim = 100
-        optimizer = Adam(0.0002, 0.5)
-        self.discriminator = self.build_discriminator()
-        self.discriminator.compile(loss='binary_crossentropy',
-                                   optimizer=optimizer,
-                                   metrics=['accuracy'])
-        self.generator = self.build_generator()
+        self.rows = 28
+        self.cols = 28
+        self.img_shape = (self.rows, self.cols, 1)
 
-        gan_input = Input(shape=(self.latent_dim,))
+        optimizer = Adam(0.0002, 0.5)
+        self.disc = self.get_disc_nn()
+        self.disc.compile(loss='binary_crossentropy',
+                          optimizer=optimizer,
+                          metrics=['accuracy'])
+        self.generator = self.get_gen_nn()
+        self.input_noise_dim = 100
+
+        gan_input = Input(shape=(self.input_noise_dim,))
         img = self.generator(gan_input)
-        self.discriminator.trainable = False
-        validity = self.discriminator(img)
+        self.disc.trainable = False
+        validity = self.disc(img)
         self.combined = Model(gan_input, validity)
         self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
 
-    def build_generator(self):
+    def add_hidden_layer(self, model, num_nodes, normalize=False, is_input=False):
+        if is_input:
+            model.add(Dense(num_nodes, input_dim=self.input_noise_dim))
+        else:
+            model.add(Dense(num_nodes))
+        model.add(LeakyReLU(alpha=0.2))
+
+        if normalize:
+            model.add(BatchNormalization(momentum=0.8))
+
+    def get_gen_nn(self):
         model = Sequential()
 
-        model.add(Dense(256, input_dim=self.latent_dim))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-
-        model.add(Dense(512))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-
-        model.add(Dense(1024))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
+        self.add_hidden_layer(model, 256, True, True)
+        self.add_hidden_layer(model, 512, True)
+        self.add_hidden_layer(model, 1024, True)
 
         model.add(Dense(np.prod(self.img_shape), activation='tanh'))
         model.add(Reshape(self.img_shape))
 
-        noise = Input(shape=(self.latent_dim,))
+        noise = Input(shape=(self.input_noise_dim,))
         img = model(noise)
 
         return Model(noise, img)
 
-    def build_discriminator(self):
+    def get_disc_nn(self):
         model = Sequential()
         model.add(Flatten(input_shape=self.img_shape))
-        model.add(Dense(512))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dense(256))
-        model.add(LeakyReLU(alpha=0.2))
+
+        self.add_hidden_layer(model, 512)
+        self.add_hidden_layer(model, 256)
+
         model.add(Dense(1, activation='sigmoid'))
 
         img = Input(shape=self.img_shape)
@@ -82,42 +84,39 @@ class GAN():
             idx = np.random.randint(0, X_train.shape[0], batch_size)
             imgs = X_train[idx]
 
-            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+            noise = np.random.normal(0, 1, (batch_size, self.input_noise_dim))
 
-            gen_imgs = self.generator.predict(noise)
-            d_loss_real = self.discriminator.train_on_batch(imgs, valid)
-            d_loss_fake = self.discriminator.train_on_batch(gen_imgs, fake)
+            output = self.generator.predict(noise)
+            d_loss_real = self.disc.train_on_batch(imgs, valid)
+            d_loss_fake = self.disc.train_on_batch(output, fake)
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
 
-            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
+            noise = np.random.normal(0, 1, (batch_size, self.input_noise_dim))
             g_loss = self.combined.train_on_batch(noise, valid)
             print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
 
             if epoch % sample_interval == 0:
-                self.sample_images(epoch)
+                self.generate_images(epoch)
 
-    def sample_images(self, epoch):
+    def generate_images(self, epoch):
+        row, column = 5, 5
+        noise = np.random.normal(0, 1, (row * column, self.input_noise_dim))
+        output = 0.5 * self.generator.predict(noise) + 0.5
 
-        r, c = 5, 5
-        noise = np.random.normal(0, 1, (r * c, self.latent_dim))
-        gen_imgs = self.generator.predict(noise)
-
-        gen_imgs = 0.5 * gen_imgs + 0.5
-
-        fig, axs = plt.subplots(r, c)
+        fig, axs = plt.subplots(row, column)
         cnt = 0
-        for i in range(r):
-            for j in range(c):
-                axs[i, j].imshow(gen_imgs[cnt, :, :, 0], cmap='gray')
+        for i in range(row):
+            for j in range(column):
+                axs[i, j].imshow(output[cnt, :, :, 0], cmap='gray')
                 axs[i, j].axis('off')
                 cnt += 1
-        fig.savefig("gan_mnist/%d.png" % epoch)
+        fig.savefig("gan/%d.png" % epoch)
         plt.close()
 
 
 def main():
-    if not os.path.exists("./gan_mnist"):
-        os.makedirs("./gan_mnist")
+    if not os.path.exists("./gan"):
+        os.makedirs("./gan")
     gan = GAN()
     gan.train(epochs=30000, batch_size=256, sample_interval=200)
 
--- vae_ffnn.py
+++ vae_ffnn.py
@@ -7,7 +7,6 @@ from keras.models import Model
 from keras import backend as K
 from keras.datasets import mnist
 
-
 batch_size = 100
 original_dim = 784
 latent_dim = 2
@@ -15,76 +14,77 @@ intermediate_dim = 256
 epochs = 50
 
 
-(x_train, y_train_), (x_test, y_test_) = mnist.load_data()
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+def build_latent_layer(args):
+    z_mean, z_log_var = args
+    epsilon = K.random_normal(shape=K.shape(z_mean))
+    return z_mean + K.exp(z_log_var / 2) * epsilon
+
+
+def get_mnist_data():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.astype('float32') / 255.
+    x_test = x_test.astype('float32') / 255.
+    x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+    x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+    return x_train, y_train, x_test, y_test
 
 
 x = Input(shape=(original_dim,))
 h = Dense(intermediate_dim, activation='relu')(x)
 
-z_mean = Dense(latent_dim)(h)
-z_log_var = Dense(latent_dim)(h)
+mean = Dense(latent_dim)(h)
+log_var = Dense(latent_dim)(h)
 
-def sampling(args):
-    z_mean, z_log_var = args
-    epsilon = K.random_normal(shape=K.shape(z_mean))
-    return z_mean + K.exp(z_log_var / 2) * epsilon
+z = Lambda(build_latent_layer, output_shape=(latent_dim,))([mean, log_var])
 
-z = Lambda(sampling, output_shape=(latent_dim,))([z_mean, z_log_var])
+# Build the hidden layer decoder, mean decoder which will then
+# transform the reduced space into original dimension
+hidden_layer_decoder = Dense(intermediate_dim, activation='relu')
+# use sigmoid because we want the probability of activation of neurons
+mean_decoder = Dense(original_dim, activation='sigmoid')
+decoded_hidden_layer = hidden_layer_decoder(z)
+decoded_mean = mean_decoder(decoded_hidden_layer)
 
-decoder_h = Dense(intermediate_dim, activation='relu')
-decoder_mean = Dense(original_dim, activation='sigmoid')
-h_decoded = decoder_h(z)
-x_decoded_mean = decoder_mean(h_decoded)
+vae = Model(x, decoded_mean)
 
-vae = Model(x, x_decoded_mean)
-
-xent_loss = K.sum(K.binary_crossentropy(x, x_decoded_mean), axis=-1)
-kl_loss = - 0.5 * K.sum(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
-vae_loss = K.mean(xent_loss + kl_loss)
+# calculate cross entropy and reconstruction loss
+binary_cross_entropy = K.sum(K.binary_crossentropy(x, decoded_mean), axis=-1)
+kl_loss = - 0.5 * K.sum(1 + log_var - K.square(mean) - K.exp(log_var), axis=-1)
+vae_loss = K.mean(binary_cross_entropy + kl_loss)
 
 vae.add_loss(vae_loss)
 vae.compile(optimizer='rmsprop')
 vae.summary()
+x_train, _, x_test, _ = get_mnist_data()
+vae.fit(x_train, shuffle=True, epochs=epochs, batch_size=batch_size, validation_data=(x_test, None))
 
-vae.fit(x_train,
-        shuffle=True,
-        epochs=epochs,
-        batch_size=batch_size,
-        validation_data=(x_test, None))
+encoder = Model(x, mean)
 
 
-encoder = Model(x, z_mean)
+def main():
+    decoder_input = Input(shape=(latent_dim,))
+    _h_decoded = hidden_layer_decoder(decoder_input)
+    _x_decoded_mean = mean_decoder(_h_decoded)
+    generator = Model(decoder_input, _x_decoded_mean)
 
-x_test_encoded = encoder.predict(x_test, batch_size=batch_size)
-plt.figure(figsize=(6, 6))
-plt.scatter(x_test_encoded[:, 0], x_test_encoded[:, 1], c=y_test_)
-plt.colorbar()
-plt.show()
+    n = 15
+    digit_size = 28
+    figure = np.zeros((digit_size * n, digit_size * n))
 
-decoder_input = Input(shape=(latent_dim,))
-_h_decoded = decoder_h(decoder_input)
-_x_decoded_mean = decoder_mean(_h_decoded)
-generator = Model(decoder_input, _x_decoded_mean)
+    grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
+    grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
 
-n = 15
-digit_size = 28
-figure = np.zeros((digit_size * n, digit_size * n))
+    for i, yi in enumerate(grid_x):
+        for j, xi in enumerate(grid_y):
+            z_sample = np.array([[xi, yi]])
+            x_decoded = generator.predict(z_sample)
+            digit = x_decoded[0].reshape(digit_size, digit_size)
+            figure[i * digit_size: (i + 1) * digit_size, j * digit_size: (j + 1) * digit_size] = digit
 
-grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
-grid_y = norm.ppf(np.linspace(0.05, 0.95, n))
+    plt.figure(figsize=(10, 10))
+    plt.imshow(figure, cmap='Greys_r')
+    plt.show()
 
-for i, yi in enumerate(grid_x):
-    for j, xi in enumerate(grid_y):
-        z_sample = np.array([[xi, yi]])
-        x_decoded = generator.predict(z_sample)
-        digit = x_decoded[0].reshape(digit_size, digit_size)
-        figure[i * digit_size: (i + 1) * digit_size,
-               j * digit_size: (j + 1) * digit_size] = digit
 
-plt.figure(figsize=(10, 10))
-plt.imshow(figure, cmap='Greys_r')
-plt.show()
+if __name__ == '__main__':
+    main()
